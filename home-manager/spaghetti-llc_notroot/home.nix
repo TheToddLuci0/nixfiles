4b7d626ab7f613deb77838effa9154379b7fb55d
Test Haxor <26094248+TheToddLuci0@users.noreply.github.com>
2026-01-14 15:07:48 -0600

    nix fmt

--- home-manager/spaghetti-llc_notroot/home.nix
+++ home-manager/spaghetti-llc_notroot/home.nix
@@ -198,7 +198,6 @@
 
   programs.kitty.enable = true;
 
-
   # Moved here since this is the only place that actually has the flake imported
   programs.steam.rocksmithPatch.enable = true; # https://github.com/theNizo/linux_rocksmith/blob/main/guides/setup/nixos/flake.md
 }
