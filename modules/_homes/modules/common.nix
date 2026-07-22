36e032f6beafd0c7cf2ed58ad2c12a8075ddd0b7
TheToddLuci0 <26094248+TheToddLuci0@users.noreply.github.com>
2026-07-22 11:02:47 -0500

    updates, move to hm programs

--- modules/_homes/modules/common.nix
+++ modules/_homes/modules/common.nix
@@ -1,7 +1,10 @@
 {pkgs, ...}: {
   # Configuration for all hosts.
-  home.packages = with pkgs; [
-    ripgrep
-    uv
-  ];
+  # home.packages = with pkgs; [
+  #   ripgrep
+  #   uv
+  # ];
+  programs.uv.enable = true;
+  programs.ripgrep.enable = true;
+  programs.fd.enable = true;
 }
