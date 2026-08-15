e7e4dfef86366b0aa135221d5eaba2365c2c3db3
TheToddLuci0 <thetoddluci0@pm.me>
2026-08-15 12:28:01 -0500

    Den full of Spaghettii

--- modules/hosts/spaghetti-llc.nix
+++ modules/hosts/spaghetti-llc.nix
@@ -1,6 +1,9 @@
-{den, ...}: {
+{den, inputs, ...}: {
 
   den.aspects.spaghetti-llc = {
-    nixos.imports = [../_nixos/hosts/spaghetti-llc/configuration.nix];
+    nixos.imports = [
+      ../_nixos/hosts/spaghetti-llc/configuration.nix
+      inputs.nixos-hardware.nixosModules.dell-xps-15-9570-nvidia
+    ];
   };
 }
---
+++ modules/nixos-hardware.nix
@@ -0,0 +1,10 @@
+{ inputs, flake-file, den,  ... }: {
+
+  flake-file.inputs = {
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+}
