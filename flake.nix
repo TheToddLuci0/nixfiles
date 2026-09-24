aedbbc33b47adf6a72818d8dc17cd91f287350c8
TheToddLuci0 <26094248+TheToddLuci0@users.noreply.github.com>
2026-09-24 10:30:58 -0500

    Move den to stable, not main

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
-    den.url = "github:denful/den";
+    den.url = "github:denful/den?ref=latest";
     direnv-instant = {
       url = "github:Mic92/direnv-instant";
       inputs.nixpkgs.follows = "nixpkgs";
